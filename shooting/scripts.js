cf14c0992e3312efa4984f5413d8f85b32e342f9
m0use-dev <114124358+m0use-dev@users.noreply.github.com>
2023-03-19 02:19:16 +0000

    敵追加とゲームオーバー条件を追加

--- shooting/scripts.js
+++ shooting/scripts.js
@@ -37,8 +37,45 @@ class Bullet {
   }
 }
 
+class Enemy {
+  constructor(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+  }
+
+  draw() {
+    ctx.fillStyle = "red";
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+
+  update() {
+    this.y += 1;
+  }
+}
+
 const spaceship = new Spaceship(canvas.width / 2 - 25, canvas.height - 50, 50, 50);
 const bullets = [];
+const enemies = [];
+
+function checkCollision(a, b) {
+  return (
+    a.x < b.x + b.width &&
+    a.x + a.width > b.x &&
+    a.y < b.y + b.height &&
+    a.y + a.height > b.y
+  );
+}
+
+function spawnEnemy() {
+  const x = Math.random() * (canvas.width - 50);
+  const enemy = new Enemy(x, 0, 50, 50);
+  enemies.push(enemy);
+  setTimeout(spawnEnemy, 2000);
+}
+
+spawnEnemy();
 
 function update() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -53,6 +90,42 @@ function update() {
     if (bullet.y < 0) {
       bullets.splice(i, 1);
       i--;
+    }
+  }
+
+  for (let i = 0; i < enemies.length; i++) {
+    const enemy = enemies[i];
+    enemy.update();
+    enemy.draw();
+
+    if (enemy.y > canvas.height) {
+      enemies.splice(i, 1);
+      i--;
+    }
+  }
+
+  for (let i = 0; i < bullets.length; i++) {
+    const bullet = bullets[i];
+
+    for (let j = 0; j < enemies.length; j++) {
+      const enemy = enemies[j];
+
+      if (checkCollision(bullet, enemy)) {
+        bullets.splice(i, 1);
+        i--;
+        enemies.splice(j, 1);
+        j--;
+        break;
+      }
+    }
+  }
+
+  for (let i = 0; i < enemies.length; i++) {
+    const enemy = enemies[i];
+
+    if (checkCollision(spaceship, enemy)) {
+      console.log("Game Over");
+      return;
     }
   }
 
